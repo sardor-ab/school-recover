1dee5ca5d72db74cc1b057fa1073a5223d7b15ad
Sardor <alijanov.sb@gmail.com>
2022-06-13 19:02:24 +0500

    feat: courses done

--- js/index.js
+++ js/index.js
@@ -60,6 +60,30 @@ const extras = [
   },
 ];
 
+const courses = [
+  {
+    id: "course-0",
+    title: "Mathematics",
+    description:
+      "If a teenager is difficult with mathematics, this does not mean at all that he has no abilities - we just need to find a clear explanation with a clear example from life.",
+    image: "./images/annie-spratt-math-unsplash.jpg",
+  },
+  {
+    id: "course-1",
+    title: "Intro to IT",
+    description:
+      "The IT industry is growing quickly as companies become more technology-based and the need to manage information becomes increasingly vital. Introduction to IT is geared towards complete beginners looking to take their first steps into the world of Information Technology.",
+    image: "./images/jeswin-thomas-it-unsplash.jpg",
+  },
+  {
+    id: "course-2",
+    title: "Robotics",
+    description:
+      "The future is in the hands of our children. They are meant to change the world for the better. In the age of rapidly developing technologies, the field of robotics is gaining more and more momentum.",
+    image: "./images/jeswin-thomas-robotics-unsplash.jpg",
+  },
+];
+
 function createFeature(id, icon, title, description) {
   return `
   <div class="feature" id=${id}>
@@ -78,6 +102,21 @@ function createExtra(id, upperContent, lowerContent) {
   <div class="extra__container-item" id=${id}>
     <span class="extra__container-item-top">${upperContent}</span>
     <span class="extra__container-item-bottom">${lowerContent}</span>
+  </div>`;
+}
+
+function createCourse(id, title, description, image) {
+  return `
+  <div class="course" id=${id}>
+    <div class="course__container">
+      <div class="course__container-image">
+        <img src="${image}" alt="Course image" class="course-image"/>
+      </div>
+      <div class="course__container-content">
+        <h3 class="course__container-content-title">${title}</h3>
+        <p class="course__container-content-description">${description}</p>
+      </div>
+    </div>
   </div>`;
 }
 
@@ -100,6 +139,14 @@ $(function () {
   extras.forEach((extra) => {
     $(".extra__container").append(
       $(createExtra(extra.id, extra.upperContent, extra.lowerContent))
+    );
+  });
+});
+
+$(function () {
+  courses.forEach((course) => {
+    $(".products__wrapper-lower").append(
+      $(createCourse(course.id, course.title, course.description, course.image))
     );
   });
 });
