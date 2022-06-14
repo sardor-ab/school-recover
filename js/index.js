e8330727657f218e19c0a78f2db17da1359ef2a7
Sardor <alijanov.sb@gmail.com>
2022-06-14 14:31:57 +0500

    refactor: scroll adjust

--- js/index.js
+++ js/index.js
@@ -206,3 +206,53 @@ $(function () {
     );
   });
 });
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $(".site-header").addClass("fixed-header");
+    }
+  });
+});
+
+$(".button").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#join__form").offset().top,
+    },
+    2000
+  );
+});
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              return false;
+            } else {
+              $target.attr("tabindex", "-1");
+              $target.focus();
+            }
+          }
+        );
+      }
+    }
+  });
